c72a1cad1282528072a8ce720e4735044dfc433d
Aman <amanmalviya14032000@gmail.com>
2022-02-05 15:34:42 +0530

    Insertion of variables enabled

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from ast import Pass
 import smtplib
 from email.message import EmailMessage
 import xlrd
 from dotenv import dotenv_values
+from mako.template import Template
+
 config = dotenv_values(".env")
 EMAIL_ADDRESS = config['EMAIL']
 EMAIL_PASSWORD = config['PASSWORD']
@@ -10,8 +11,7 @@ EMAIL_PASSWORD = config['PASSWORD']
 wb = xlrd.open_workbook("./sheet.xlsx")
 sheet = wb.sheet_by_index(0)
 
-with open("./chetna.html", "r", encoding="utf-8") as f:
-    body = f.read()
+body = Template(filename='./thankyou.html')
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
@@ -21,7 +21,7 @@ with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
         msg['Subject'] = "Invitation to Anubhuti '22"
         msg['From'] = EMAIL_ADDRESS
         msg['To'] = reciever
-        msg.set_content('This is a plain text email')
-        msg.add_alternative(body, subtype='html')
+        msg.set_content('This is an email')
+        msg.add_alternative(body.render(name=sheet.cell_value(i, 1)), subtype='html')
         smtp.send_message(msg)
         print("Mail sent to " + reciever)
